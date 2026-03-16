9af680cbf35faae9867242d3b8479eee835f073a
GOMEZz157 <nicksss.gomes@gmail.com>
2026-03-16 11:35:34 -0300

    js pronto e calculadora funcionando

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,23 @@
-console.log("teste")
+// Seleção de elementos
+const display = document.querySelector("#display");
+
+// Funções
+const insertToDisplay = (data) => {
+  display.value += data;
+};
+
+const clearAll = () => {
+  display.value = "";
+};
+
+const deleteLast = () => {
+  display.value = display.value.slice(0, -1);
+};
+
+const calculateResult = () => {
+  try {
+    display.value = eval(display.value)
+  } catch (err) {
+    display.value("Error")
+  }
+};
